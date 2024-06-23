1666d1a89cdb2031f9a9746b4fe9df79a0b31018
jinho <jinho0547@naver.com>
2024-06-23 12:53:07 +0900

    20240623

---
+++ programmers/T12915.java
@@ -0,0 +1,13 @@
+class T {
+    public static void main(String[] args) {
+        int result = solution();
+
+        System.out.println(result);
+    }
+    
+    static int solution(){
+        int answer = 0;
+
+        return answer;
+    }
+}
