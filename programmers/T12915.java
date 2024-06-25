124caf0e4c6011277ec63667323ee2dedaa8e57a
jinho <jinho0547@naver.com>
2024-06-25 11:13:39 +0900

    20240625

--- programmers/T12915.java
+++ programmers/T12915.java
@@ -1,13 +1,24 @@
 class T {
     public static void main(String[] args) {
-        int result = solution();
+        String[] strings = {"sun","bed", "car"};
+        int n = 1;
+        String[] result = solution(strings, n);
 
-        System.out.println(result);
+        System.out.println(result.toString());
     }
+    /*
+     * 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 
+     * 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다. 
+     * 
+     * 예를 들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1의 문자 "u", "e", "a"로 strings를 정렬합니다.
+     */
     
-    static int solution(){
-        int answer = 0;
+    static String[] solution(String[] strings, int n){
 
+        String[] answer = new String[strings.length];
+
+
+        
         return answer;
     }
 }
